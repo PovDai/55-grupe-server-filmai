766ce9b330481cd3de811776b5d58808fb4c8d65
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-06-30 10:44:52 +0300

    Demo env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import { port } from './env.js';
 
-const app = express()
-const port = 3000
+const app = express();
+
+
 
 app.use(express.static('public'))
 
@@ -13,5 +15,5 @@ app.get('/', (req, res) => {
 app.get('*error', (req, res) => { res.send('Error page') })
 
 app.listen(port, () => {
-    console.log(`Server is running on: http://localhost:${port}`);
+    console.log(`WEB URL on: http://localhost:${port}`);
   })
