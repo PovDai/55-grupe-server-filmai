7307f1a129ddffc58a00f77208fe67a6c5361be9
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-07-07 09:12:40 +0300

    FILTERS

--- src/api/public/postRegister.js
+++ src/api/public/postRegister.js
@@ -17,20 +17,64 @@ export async function postRegister(req, res) {/// async asinkronine funkcija sin
     }
     const { username, email, password } = req.body;
 
+    try {
+        const sql = 'SELECT  * FROM  users WHERE username= ? OR email= ?;'; // konkreciai irasymo eilute i db nes insertas
+        const [response] = await connection.execute(sql, [username, email]) 
+
+        console.log(response)
+
+        if (response.length > 0) {
+            return res.status(400).json({
+                status: 'error',
+                msg: 'Toks vartotojas jau uzregistruotas'
+            });
+        }
+    }
+
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: 'error',
+            msg: 'Serverio klaida',
+        });
+    
+}
+
+
+
+
     try { /// try apsauga no kodo kuris gali crashint ir sustabdyti serveri.
         const sql = `INSERT INTO users (username, email, password) VALUES (?, ?, ? );`; // konkreciai irasymo eilute i db nes insertas
 
-        const response = await connection.execute(sql, [username, email, password]) // per cia irasomna i duomenu baze registruojami duomenys 
+        const response = await connection.execute(sql, [username, email, password]) // per cia irasomna i duomenu baze registruojami duomenys
+        if (response.affectedRows !== 1) {
+            return res.status(500).json({
+                status: 'error',
+                msg: 'Serverio klaida'
+            });
+        }
         
+
+
         console.log(response);
         
     } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') {
+            return res.status(500).json({
+                status: 'error',
+                msg:'kartojasi irasas'
+            })
+        }
 
         console.log(error);
+        return res.status(500).json({
+            status: 'error',
+            msg:'Serverio klaida',
+        })
         
     }
 
-    return res.json({
+    return res.status(201).json({
         status: 'success',
         msg: 'Sekminga registracija',
     });
