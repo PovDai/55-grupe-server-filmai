828e6c57616aa5a9c7625439d8be651654b51b64
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-07-08 11:26:30 +0300

    dashboard added login register update

--- src/pages/public/Home.js
+++ src/pages/public/Home.js
@@ -1,8 +1,11 @@
-
 import { PageTemplate } from "../../templates/PageTemplate.js";
 import { heroSection } from "../../ui/hero.js";
 
 export class PageHome extends PageTemplate {
+    constructor(req) {
+        super(req);
+    }
+
     main() {
         return `
             <main>
